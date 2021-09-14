2d2a88acd3a9278b6121f6eab1cb4a4dd9d3680d
ionutcarp <61273527+ionutcarp@users.noreply.github.com>
2021-09-14 18:00:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,6 +54,7 @@ module.exports = {
      * property can be used
      */
     //userDir: '/home/nol/.node-red/',
+    userDir: './',
 
     /** Node-RED scans the `nodes` directory in the userDir to find local node files.
      * The following property can be used to specify an additional directory to scan.
